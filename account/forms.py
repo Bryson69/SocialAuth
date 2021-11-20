b164fe96ea15da5a29128626a2dc47b6dc5d905f
Bryson69 <brysonmundia@gmail.com>
2021-11-20 12:51:46 +0300

    Update Forms.py file

--- account/forms.py
+++ account/forms.py
@@ -3,24 +3,37 @@ from django.contrib.auth import load_backend
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.forms import fields
-from django.forms.widgets import ClearableFileInput
+from django.forms import widgets
+from django.forms.widgets import ClearableFileInput, PasswordInput
+from django.http.request import RawPostDataException
+
+from account.models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-class UserRegisterationForm(forms.ModelForm):
-    password = forms.CharField(label = "Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label= "Repeat Password", widget= forms)
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=PasswordInput)
+    password2 = forms.CharField(label="Repeat Password", widget=PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
 
+
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd ['password2']:
+        if cd['password'] != cd['password2']:
             raise ValidationError("Password Don\'t match")
         return cd['password2']
 
-  
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
